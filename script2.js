b6ab7c1ce4a86dabe74e9eedc69083e8d90720e2
Sportfly <64900506+crazymemesmaker@users.noreply.github.com>
2022-01-27 21:31:10 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,12 +1,12 @@
 //Alerts warning
-setTimeout(alert2, 1000);
+setTimeout(alert2, 2000);
 document.getElementById("div1").style.visibility = "hidden";
 function alert2() {
 alert("⚠️ WARNING: This game is an horror game and can scare kids or people that are not for horror.\n\n This game is marked NFE (Not for everyone), if you're not for horror please leave. \n\n For people that are sure what they're playing, thanks for playing this game.");
 document.getElementById("div1").style.visibility = "visible";
 document.getElementById("body").style.backgroundImage = "url('')";
 }
-//Gets actual year and sets copyright
+//Gets actual year and sets copyright text
 const date = new Date();
 let year = date.getFullYear();
 document.getElementById("p1").innerText = "Copyright © 2021 / " + year + ".";
