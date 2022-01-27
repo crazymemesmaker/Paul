8bb91585035322039e797f2ed5231f5ccc0f5ab4
Sportfly <64900506+crazymemesmaker@users.noreply.github.com>
2022-01-27 15:22:14 +0100

    Create DetectDevice.js

---
+++ DetectDevice.js
@@ -0,0 +1,14 @@
+function block() {
+alert("You can't play this game in mobile without desktop site. Go and enable desktop site and try again.");
+document.querySelector("*").style.visibility = "hidden";
+document.write("Put desktop site and try again to play. If you don't know how to put it just go see a tutorial. Desktop site is obligatory to play the game better.");
+}
+    const ua = navigator.userAgent;
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+        block();
+    }
+    else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+        block();
+    } else {
+    
+    }
