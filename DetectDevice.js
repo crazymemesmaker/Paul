07fcb62e36adc10a3523117985eb053d13824457
Sportfly <64900506+crazymemesmaker@users.noreply.github.com>
2022-01-27 17:02:57 +0100

    Update DetectDevice.js

--- DetectDevice.js
+++ DetectDevice.js
@@ -4,12 +4,13 @@ alert("You can't play this game in mobile without desktop site. Go and enable de
 document.write("Put desktop site and try again to play. If you don't know how to put it just go see a tutorial. Desktop site is obligatory to play the game better.");
 block = "block";
 }
+//Detects device
     const ua = navigator.userAgent;
     if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
-        block();
+        block(); //Tablet
     }
     else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
-        block();
+        block(); //Phone
     } else {
-    
+     //Computer
     }
