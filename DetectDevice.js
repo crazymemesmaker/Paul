f9270775af3b0e144c4bd1faa56fbeca13ef0723
Sportfly <64900506+crazymemesmaker@users.noreply.github.com>
2022-02-03 16:43:09 +0100

    Update DetectDevice.js

--- DetectDevice.js
+++ DetectDevice.js
@@ -1,5 +1,6 @@
 var block;
 function block() {
+var audio = new Audio("https://www.myinstants.com/media/sounds/annihilation-the-alien.mp3").play;
 alert("You can't play this game in mobile without desktop site. Go and enable desktop site and try again.");
 document.write("Put desktop site and try again to play. If you don't know how to put it just go see a tutorial. Desktop site is obligatory to play the game better. Also thanks for playing.");
 block = "block";
