5bd2fe9fff04407a9e5642db3f621f49d9fbba9a
Sportfly <64900506+crazymemesmaker@users.noreply.github.com>
2022-02-05 08:57:42 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -6,7 +6,4 @@ alert("⚠️ WARNING: This game is an horror game and can scare kids or people 
 document.getElementById("div1").style.visibility = "visible";
 document.getElementById("body").style.backgroundImage = "url('')";
 }
-//Gets actual year and sets copyright text
-const date = new Date();
-let year = date.getFullYear();
-document.getElementById("p1").innerText = "Copyright © 2021 / " + year + ".";
+
